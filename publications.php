a7bd7c8bba859a3046c8e4567b49b3ea5c5d3874
Nic Winn <nic.liamg@gmail.com>
2017-02-17 13:32:15 -0600

    new page

---
+++ publications.php
@@ -0,0 +1,18 @@
+<?php
+$pagename = 'Publications';
+include("template-top.php");
+?>
+<div id="content" class="row">
+    <div class="columns">
+        <h1><?php echo $pagename; ?></h1>
+        <ul>
+            <li>
+                Bailey JE, Surbhi S, Bell PC, Jones AM, Rashed S, Ugwueke MO.  SafeMed: Utilizing Pharmacy Technicians in a Novel Role as Community Health Workers to Improve Transitions of Care. <em>J Am Pharm Assoc</em>. 2016;56(1):73-81, doi:10.1016/j.japh.2015.11.011
+            </li>
+            <li>
+                Harris LJ, Graetz I, Podila PSB, Wan J, Waters TM, Bailey JE. Characteristics of Hospital and Emergency Care Super-utilizers with Multiple Chronic Conditions, <em>J Emerg Med</em>, 2015 Oct 12. doi: 10.1016/j.jemermed.2015.09.002 [Epub ahead of print]
+            </li>
+        </ul>
+    </div>
+</div>
+<?php include("template-bottom.php"); ?>
